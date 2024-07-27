b8711319ef31546337f8d3a2c83204d7111c1e63
haruki7049 <tontonkirikiri@gmail.com>
2024-07-27 09:04:18 +0900

    style: Update flake.nix

--- flake.nix
+++ flake.nix
@@ -5,30 +5,23 @@
     treefmt-nix.url = "github:numtide/treefmt-nix";
   };
 
-  outputs =
-    { self
-    , nixpkgs
-    , flake-utils
-    , treefmt-nix
-    , ...
-    }:
+  outputs = { self, nixpkgs, flake-utils, treefmt-nix, ... }:
     flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = import nixpkgs {
-        inherit system;
-      };
-      treefmtEval = treefmt-nix.lib.evalModule pkgs ./treefmt.nix;
-    in
-    {
-      formatter = treefmtEval.config.build.wrapper;
-      checks.formatting = treefmtEval.config.build.check self;
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+        treefmtEval = treefmt-nix.lib.evalModule pkgs ./treefmt.nix;
+      in
+      {
+        formatter = treefmtEval.config.build.wrapper;
+        checks.formatting = treefmtEval.config.build.check self;
 
-      devShells.default = pkgs.mkShell {
-        packages = [
-          pkgs.nil
-          pkgs.nixpkgs-fmt
-        ];
-      };
-    }
-    );
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.nil
+            pkgs.nixpkgs-fmt
+          ];
+        };
+      });
 }
