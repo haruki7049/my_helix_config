aaf3501c0686d5ca2da5fc2e97497993b842e0c0
haruki7049 <tontonkirikiri@gmail.com>
2025-01-07 21:33:16 +0900

    chore(flakes): Change flake-utils to flake-parts

--- flake.nix
+++ flake.nix
@@ -1,21 +1,32 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-    treefmt-nix.url = "github:numtide/treefmt-nix";
+    systems.url = "github:nix-systems/default";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, treefmt-nix, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
+      imports = [
+        inputs.treefmt-nix.flakeModule
+      ];
+
+      perSystem = { pkgs, ... }: {
+        treefmt = {
+          projectRootFile = "flake.nix";
+          programs.nixpkgs-fmt.enable = true;
+          programs.taplo.enable = true;
+          programs.actionlint.enable = true;
         };
-        treefmtEval = treefmt-nix.lib.evalModule pkgs ./treefmt.nix;
-      in
-      {
-        formatter = treefmtEval.config.build.wrapper;
-        checks.formatting = treefmtEval.config.build.check self;
 
         devShells.default = pkgs.mkShell {
           packages = [
@@ -23,5 +34,6 @@
             pkgs.nixpkgs-fmt
           ];
         };
-      });
+      };
+    };
 }
--- treefmt.nix
+++
@@ -1,6 +0,0 @@
-{
-  projectRootFile = "flake.nix";
-  programs.nixpkgs-fmt.enable = true;
-  programs.taplo.enable = true;
-  programs.actionlint.enable = true;
-}
